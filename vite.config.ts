e37f67e64ebb905e119259960c6fd83743fd17fd
xieyajie <xyjxyf@163.com>
2026-03-05 10:36:30 +0800

    修复网站不显示

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/braveray/',
+  base: '/braveray-outsourcing-site/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {

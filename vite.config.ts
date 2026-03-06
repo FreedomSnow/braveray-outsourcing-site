1c554ee392a4efd042eb1bf932b80e1713a91be9
xieyajie <xyjxyf@163.com>
2026-03-06 17:59:10 +0800

    更新网站标题

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/braveray-outsourcing-site/',
+  base: '/customization/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
